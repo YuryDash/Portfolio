97df1e6f7917ea5f82286c86bd23474efe4b89a4
Yury Daszukiewicz <ydaszukiewicz@gmail.com>
2023-07-10 13:54:41 +0300

    Add @media for contacts

--- src/Features/Main/Main.tsx
+++ src/Features/Main/Main.tsx
@@ -1,28 +1,24 @@
-import s from "./Main.module.scss"
-import {TitleBold} from "../../components/TitleBold/TitleBold";
-import {Particle} from "./Particle/Particle";
-
+import s from "./Main.module.scss";
+import { TitleBold } from "../../components/TitleBold/TitleBold";
+import { Particle } from "./Particle/Particle";
 
 export const Main = () => {
-    return (
-        <div className={s.main} id={'main'}>
-            <Particle/>
-            <TitleBold text={'ABOUT'} colorText={'ME'}/>
-            <div className={s.main__content}>
-
-                <div className={s.main__content_item}>
-                    <p>
-                            I DESIGN AND CODE BEAUTIFUL THINGS, AND I LOVE WHAT I DO.
-                    </p>
-                </div>
-                <div className={ `${ s.main__content_item } + ${s.main__content_photo}` }>
-                    <img src='https://www.meme-arsenal.com/memes/a9f034df5285bed728c5fe025c01e1d3.jpg' alt="Just MyAva"/>
-                </div>
-
-            </div>
-            <div>
-
-            </div>
+  return (
+    <div className={s.main} id={"main"}>
+      <Particle />
+      <TitleBold text={"ABOUT"} colorText={"ME"} />
+      <div className={s.main__content}>
+        <div className={s.main__content_item}>
+          <p>I DESIGN AND CODE BEAUTIFUL THINGS, AND I LOVE WHAT I DO.</p>
         </div>
-    )
-}
+        <div className={`${s.main__content_item} + ${s.main__content_photo}`}>
+          <img
+            className={s.main__content_img}
+            src="https://citaty.info/files/styles/poster/public/characters/633.jpg?itok=qAGPclX6"
+            alt="Just MyAva"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
